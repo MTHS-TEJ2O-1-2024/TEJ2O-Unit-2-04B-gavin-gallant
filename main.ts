cb5cf2a31a9a0406cabb2a4d87896b8833b16913
gavin-gallant <gavin.gallant@mths.ca>
2024-09-19 11:14:34 -0400

    made the code show the temperature

--- main.ts
+++ main.ts
@@ -1,8 +1,18 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: gavin gallant
+ * Created on: Sep 2024
+ * This program says the tempeture 
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.pause(1000)
+
+
+let tempeture = 0 
+
+input.onButtonPressed(Button.A, function () {
+    tempeture = input.temperature()
+    basic.showString('Temp=')
+    basic.showNumber(tempeture)
+})
